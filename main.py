15dba29de840e32816406d0ffadf63321963277e
Marco Rossel <andresmarcorossel@gmail.com>
2023-08-29 16:51:41 -0400

    python api | 02

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"Hello": "Worlddddd"}
+
+@app.get("/home")
+def home_page():
+    return {'home page': 'some page in the future'}
+
+@app.post("/createposts")
+def create_posts(x_var_name: dict = Body(...) ):
+    print(x_var_name)
+    return {f"{x_var_name['title']}": f"body : {x_var_name['body']}"}
