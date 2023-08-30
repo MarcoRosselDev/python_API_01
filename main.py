72485a6ed4413e2907208f6a89c9631fbbf541d6
Marco Rossel <andresmarcorossel@gmail.com>
2023-08-29 20:07:49 -0400

    python api | 05

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Body
 from pydantic import BaseModel
 from typing import Optional
+import random
 
 app = FastAPI()
 
@@ -12,17 +13,28 @@ class Post(BaseModel):  #---> Modelo imortado de pydantic que valida el formato 
 
 my_posts = [{'title':'my first post', 'content': 'content of my first post', 'id': 1}, {'title':'my second post', 'content': 'content of my second post', 'id': 2}]
 
+def get_post(id):
+    for i in my_posts:
+        if i['id'] == id:
+            return i
+
 @app.get("/")
 def read_root():
     return {"Hello": "Worlddddd"}
 
 @app.get("/myposts")
 def myposts():
-    return {'posts': my_posts }
+    return {'data': my_posts }
 
 @app.post("/posts")
 def create_posts(x_var_name: Post ):
-    x_var_name = dict(x_var_name)
-    x_var_name['id'] = 76152
-    my_posts.append(x_var_name)
-    return {"title": my_posts}
+    #x_var_name = dict(x_var_name)
+    post_dict = dict(x_var_name)
+    post_dict['id'] = int(random.randint(0, 1000))
+    my_posts.append(post_dict)
+    return {"data": my_posts}
+
+@app.get("/posts/{id}")
+def get_one_post(id:int):
+    resp =  get_post(id)
+    return {'post_detail': resp}
