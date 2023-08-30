3723afe6baa74735efe6242e06a9a399b06dfdf1
Marco Rossel <andresmarcorossel@gmail.com>
2023-08-30 12:24:09 -0400

    python api | 08

--- main.py
+++ main.py
@@ -55,3 +55,15 @@ def delete_posts(id:int):
 
     my_posts.pop(index)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+@app.put("/posts/{id}")
+def update_post(id:int, x_post:Post):
+    index = found_id(id)
+
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id: {id} does not exist.")
+    post_dict = x_post.dict()
+    post_dict['id'] = id
+    my_posts[index] = post_dict
+
+    return {'data': post_dict}
