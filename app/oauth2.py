807fee792a15851b997bc90202a46d61ed97ef81
Marco Rossel <andresmarcorossel@gmail.com>
2023-09-07 01:30:05 -0300

    python api | authentication required to do everyting

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,9 +1,12 @@
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
 from . import schemas
+from fastapi import Depends, status, HTTPException
+from fastapi.security import OAuth2PasswordBearer
 from decouple import config # .env config
 secret_key_env = config('SECRET_KEY')
 algorithm_env = config('ALGORITHM')
+oauth2_scheme=OAuth2PasswordBearer(tokenUrl='login')
 
 # secret key
 # algorithm
@@ -11,21 +14,26 @@ algorithm_env = config('ALGORITHM')
 
 SECRET_KEY = secret_key_env
 ALGORITHM = algorithm_env
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+ACCESS_TOKEN_EXPIRE_MINUTES = 1440
 
 def create_access_token(data: dict):
     to_encode = data.copy()
-    expire = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
 def verify_access_token(token:str, credentials_exeption):
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         id_user:str = payload.get("user_id")
         if id_user is None:
             raise credentials_exeption
         token_data = schemas.TokenData(id_user=id)
     except JWTError:
         raise credentials_exeption
+    return token_data
+
+def get_current_user(token:str=Depends(oauth2_scheme)):
+    credentials_exception=HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=f'Could not validate credentials', headers={"WWW-Authenticate": "Bearer"})
+    return verify_access_token(token, credentials_exception)
--- app/routers/post.py
+++ app/routers/post.py
@@ -1,5 +1,5 @@
 from fastapi import Response, status, HTTPException, Depends, APIRouter
-from .. import models, schemas
+from .. import models, schemas, oauth2
 from sqlalchemy.orm import Session
 from typing import List
 from .get_db_fun import get_db
@@ -7,12 +7,12 @@ from .get_db_fun import get_db
 router = APIRouter(prefix="/posts", tags=["Post"])
 
 @router.get("/", response_model=List[schemas.Post])
-def myposts(db: Session = Depends(get_db)):
+def myposts(db: Session = Depends(get_db), user_id:int=Depends(oauth2.get_current_user)):
     posts = db.query(models.Post).all()
     return posts
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
-def create_posts(x_var_name: schemas.PostCreate, db: Session = Depends(get_db)):
+def create_posts(x_var_name: schemas.PostCreate, db: Session = Depends(get_db), user_id:int=Depends(oauth2.get_current_user)):
     new_post = models.Post(**x_var_name.dict())
     db.add(new_post)
     db.commit()
@@ -20,14 +20,14 @@ def create_posts(x_var_name: schemas.PostCreate, db: Session = Depends(get_db)):
     return new_post
 
 @router.get("/{id}", response_model=schemas.Post)
-def get_one_post(id:int, db: Session = Depends(get_db)):
+def get_one_post(id:int, db: Session = Depends(get_db), user_id:int=Depends(oauth2.get_current_user)):
     post = db.query(models.Post).filter(models.Post.id == id).first()
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'post with id: {id} was not found')
     return post
 
 @router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
-def delete_posts(id:int, db: Session = Depends(get_db)):
+def delete_posts(id:int, db: Session = Depends(get_db), user_id:int=Depends(oauth2.get_current_user)):
     post = db.query(models.Post).filter(models.Post.id == id)
     if post.first() == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id: {id} does not exist.")
@@ -38,7 +38,7 @@ def delete_posts(id:int, db: Session = Depends(get_db)):
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 @router.put("/{id}", response_model=schemas.Post)
-def update_post(id:int, x_post: schemas.PostCreate, db: Session = Depends(get_db)):
+def update_post(id:int, x_post: schemas.PostCreate, db: Session = Depends(get_db), user_id:int=Depends(oauth2.get_current_user)):
     updated_post = db.query(models.Post).filter(models.Post.id == id)
     post = updated_post.first()
 
