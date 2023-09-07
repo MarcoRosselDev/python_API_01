d1b6c4e1d1955e8c8b25fbfdaf127347ecba4a6b
Marco Rossel <andresmarcorossel@gmail.com>
2023-09-07 05:18:53 -0300

    python api | authentication required to do everyting 2

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,8 +1,10 @@
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
-from . import schemas
+from . import schemas, models
+from .routers.get_db_fun import get_db
 from fastapi import Depends, status, HTTPException
 from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
 from decouple import config # .env config
 secret_key_env = config('SECRET_KEY')
 algorithm_env = config('ALGORITHM')
@@ -34,6 +36,8 @@ def verify_access_token(token:str, credentials_exeption):
         raise credentials_exeption
     return token_data
 
-def get_current_user(token:str=Depends(oauth2_scheme)):
+def get_current_user(token:str=Depends(oauth2_scheme), db:Session = Depends(get_db)):
     credentials_exception=HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=f'Could not validate credentials', headers={"WWW-Authenticate": "Bearer"})
-    return verify_access_token(token, credentials_exception)
+    token = verify_access_token(token, credentials_exception)
+    user = db.query(models.User).filter(models.User.id == token.id).first()
+    return user
