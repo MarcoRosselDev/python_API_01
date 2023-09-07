e367f134f89e7ff18f62f2278e893a2c898c4fc7
Marco Rossel <andresmarcorossel@gmail.com>
2023-09-07 05:33:28 -0300

    python api | authentication required to do everyting 3

--- app/routers/post.py
+++ app/routers/post.py
@@ -12,9 +12,9 @@ def myposts(db: Session = Depends(get_db), user_id:int=Depends(oauth2.get_curren
     return posts
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
-def create_posts(x_var_name: schemas.PostCreate, db: Session = Depends(get_db), current_user:int=Depends(oauth2.get_current_user)):
+def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db), current_user:int=Depends(oauth2.get_current_user)):
     print(current_user)
-    new_post = models.Post(**x_var_name.dict())
+    new_post = models.Post(**post.dict())
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
