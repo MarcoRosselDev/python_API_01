1e7ffe23da664881264299e79290f2589dee29c3
Marco Rossel <andresmarcorossel@gmail.com>
2023-09-08 09:11:26 -0300

    python api | authentication required to do everyting 4

--- app/routers/post.py
+++ app/routers/post.py
@@ -7,12 +7,12 @@ from .get_db_fun import get_db
 router = APIRouter(prefix="/posts", tags=["Post"])
 
 @router.get("/", response_model=List[schemas.Post])
-def myposts(db: Session = Depends(get_db), user_id:int=Depends(oauth2.get_current_user)):
+def myposts(db: Session = Depends(get_db), user_id: int = Depends(oauth2.get_current_user)):
     posts = db.query(models.Post).all()
     return posts
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
-def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db), current_user:int=Depends(oauth2.get_current_user)):
+def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
     print(current_user)
     new_post = models.Post(**post.dict())
     db.add(new_post)
@@ -21,14 +21,14 @@ def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db), curren
     return new_post
 
 @router.get("/{id}", response_model=schemas.Post)
-def get_one_post(id:int, db: Session = Depends(get_db), current_user:int=Depends(oauth2.get_current_user)):
+def get_one_post(id:int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
     post = db.query(models.Post).filter(models.Post.id == id).first()
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'post with id: {id} was not found')
     return post
 
 @router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
-def delete_posts(id:int, db: Session = Depends(get_db), current_user:int=Depends(oauth2.get_current_user)):
+def delete_posts(id:int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
     post = db.query(models.Post).filter(models.Post.id == id)
     if post.first() == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id: {id} does not exist.")
